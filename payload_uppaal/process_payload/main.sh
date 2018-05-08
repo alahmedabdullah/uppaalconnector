a1d1a4e80f6586db6cfdcacfe65346f53d2a6342
Ahmed Abdullah <abdullah.cs@gmail.com>
2018-05-08 05:32:01 +0000

    main.sh with timings support

--- payload_uppaal/process_payload/main.sh
+++ payload_uppaal/process_payload/main.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 INPUT_DIR=$1
+OUTPUT_DIR=$2
 
 cp run.sh $INPUT_DIR/run.sh
 
@@ -9,5 +10,9 @@ RUN_DIR=`cd "$(dirname "$0")" && pwd`
 echo $RUN_DIR > mainsh.output
 
 sh $RUN_DIR/run.sh $@ 
+
+exec_end_time=`date +"%Y-%m-%d %H:%M:%S.%3N"`
+sed -i "s/EXEC_END_TIME/$exec_end_time/" ./timedata.txt
+cp ./timedata.txt $OUTPUT_DIR/timedata.txt
 
 # --- EOF ---
